d82d2c743e8990c28988b13d673fd26cc188c2c1
ihccenen <wirkz01@gmail.com>
2023-08-10 03:59:08 -0300

    added aside

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
+import Aside from './components/Aside';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -16,7 +17,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Aside />
+        <div className='content'>
+        {children}
+        </div>
+      </body>
     </html>
   );
 }
